a602b061b3fc5f19b8d5f26912db08ab8f302c42
Siddhant <Siddhant.shetty1811@gmail.com>
2024-09-02 00:25:26 +0530

    Day 2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 const Home = () => {
+    const loggedIn = { firstName: 'Siddhant' };
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions effeciently."
+                    />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalbanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+            </div>
+        </section>
     )
 }
 
